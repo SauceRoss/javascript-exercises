1afb6971c5cc8043ea8951935b2ecddec2d736ac
SauceRoss <rvmalesich@icloud.com>
2025-01-08 23:57:29 -0600

    Completed exercies 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, int) {
+    let message = ""
+    for(let i = 0; i < int; i++ ) {
+        message += string;
 
+    }
+    
+    if (int < 0){
+        message = "ERROR"
+    }
+
+    return message;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversed = "";
 
+    if(string != "") {
+        reversed = string.reverse();
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
