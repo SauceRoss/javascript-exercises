484af04df15c21e7207f0cf40fcad5e7a076fc26
SauceRoss <rvmalesich@icloud.com>
2025-01-12 15:59:54 -0600

    Completed Exercises 5,6,7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let celsius = 5/9 * (tempF - 32);
+  celsius = parseFloat(celsius.toFixed(1));
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let f = (9/5 * tempC) + 32;
+  f = parseFloat(f.toFixed(1));
+  return f;
 };
 
 // Do not edit below this line
